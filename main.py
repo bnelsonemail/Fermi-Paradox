b1d031856d7b523c1f501bb33d7b57c930a374d4
bnelson <bnelsonemail@icloud.com>
2024-09-15 18:27:05 -0400

    revisions to python code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 pi = np.pi
 galactic_disc_height = 1000 # LY
 galactic_disc_area = 7.85 * 10**9 # SQ. LY
-R_galactic_disc_radius = galactic_disc_area / galactic_disc_height
+R_galactic_disc_radius = galactic_disc_area / galactic_disc_height/2
 radio_bubble_volume_est = 4.19 * 10**6 # CU. LY
 radio_bubble_radius = 200 / 2 # LY
 
@@ -17,5 +17,5 @@ galactic_disc_volume = pi * R_galactic_disc_radius**2 * galactic_disc_height # C
 radio_bubble_volume_calc = 4/3 * pi * radio_bubble_radius**3 # CU. LY
 
 
-
+print(f"{R_galactic_disc_radius: ,.2f}")
 
---
+++ my_galaxy_simulator.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from random import randint, uniform, random
+import math
+
+# *************** MAIN INPUT ***************************
+
+# SCALED_DISC_RADIUS = disc_radius / bubble diameter
+
+# scale radio bubble diameter in light-years
+SCALE = 225 # enter 225 to see Earth's radio bubble
+
+# number of advanced civilizations from Drake equation:
+NUM_CIV = 15_600_000
+
