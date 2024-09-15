10f46e66716463beaf5959299e220633d85aef6f
bnelson <bnelsonemail@icloud.com>
2024-09-15 01:36:43 -0400

    revisions to main.py, revisions to requirements.txt and addition of documention.txt

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-# ********************* OBJECTIVE *************************
+import numpy as np
+from random import randint
+from collections import Counter
+import matplotlib.pyplot as plt
 
-# For a given number of advanced galactic civilizations and an average radio bubble size, estimate the probability of any civilization detecting the radio transmissions of any other civilization.  For perspective, post the size of Earth's curent radio bubble on a 2D graphical representation of the Milky Way.
 
 
 
-# ****************** STRATEGY *******************
+pi = np.pi
+galactic_disc_height = 1000 # LY
+galactic_disc_area = 7.85 * 10**9 # SQ. LY
+R_galactic_disc_radius = galactic_disc_area / galactic_disc_height
+radio_bubble_volume_est = 4.19 * 10**6 # CU. LY
+radio_bubble_radius = 200 / 2 # LY
 
-# 1.    Estimate the number of transmitting civilizations using the Drake equation.
-# 2.    Choose a size range for their radio bubbles.
-# 3.    Generate a formula for estimating the probability of one civilization detecting another.
-# 4.    Build a graphical model of the galaxy and post Earth's radio emissions bubble.
+galactic_disc_volume = pi * R_galactic_disc_radius**2 * galactic_disc_height # CU. LY
+radio_bubble_volume_calc = 4/3 * pi * radio_bubble_radius**3 # CU. LY
+
+
+NUM_EQUIV_VOLUMES = 1000 # number of locations in which to place civilizations
+MAX_CIVS = 5000 # maximum number of advanced civilzations 
+TRIALS = 1000 # number of times to model a given number of civilizations
+CIV_STEP_SIZE = 100 # civilizations count step size
+
+x = [] # x values for polynomial fit
+y = [] # y values for polynomial fit
+
+for num_civs in range(2, MAX_CIVS + 2, CIV_STEP_SIZE):
+    civs_per_vol = num_civs / NUM_EQUIV_VOLUMES
+    num_single_civs = 0
+    for trial in range(TRIALS):
+        locations = [] # equivalent volumes containing a civilization
+        while len(locations) < num_civs:
+            location = randint(1, NUM_EQUIV_VOLUMES)
+            locations.append(location)
+        overlap_count = Counter(locations)
+        overlap_rollup = Counter(overlap_count.values())
+        num_single_civs += overlap_rollup[1]
+    prob = 1 - (num_single_civs / (num_civs * TRIALS))
+    
+    # print ratio of civs-per-volume vs probability of 2+ civs per location
+    print("{:.4f} {:.4f}".format(civs_per_vol, prob))
+    x.append(civs_per_vol)
+    y.append(prob)
+    
+
 
 
