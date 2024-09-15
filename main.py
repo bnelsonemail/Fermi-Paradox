7d02f0ebe95d60e5837ced9ded0832c230ed52fe
bnelson <bnelsonemail@icloud.com>
2024-09-14 22:50:41 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# ********************* OBJECTIVE *************************
+
+# For a given number of advanced galactic civilizations and an average radio bubble size, estimate the probability of any civilization detecting the radio transmissions of any other civilization.  For perspective, post the size of Earth's curent radio bubble on a 2D graphical representation of the Milky Way.
+
+
+
+# ****************** STRATEGY *******************
+
+# 1.    Estimate the number of transmitting civilizations using the Drake equation.
+# 2.    Choose a size range for their radio bubbles.
+# 3.    Generate a formula for estimating the probability of one civilization detecting another.
+# 4.    Build a graphical model of the galaxy and post Earth's radio emissions bubble.
+
+
